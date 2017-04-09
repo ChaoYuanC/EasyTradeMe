990a98b9a1392fff772cde495226ce345137debb
Chao Yuan <chao.yuan@heyday.co.nz>
2017-04-10 10:34:38 +1200

    Empty

---
+++ EasyTradeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyTradeMe
+//
+//  Created by Chao Yuan on 4/10/17.
+//  Copyright © 2017 Chao Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
